f5d2395440bc562f7ed322fd7ef0b3b4a29b852b
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-03-07 16:06:26 +0000

    Tidy up tests. Give them a context

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,9 +3,6 @@ require 'gilded_rose'
 describe GildedRose do
 
   describe "#update_quality" do
-    context 'for regular items' do
-
-    end
 
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
@@ -13,19 +10,21 @@ describe GildedRose do
       expect(items[0].name).to eq "foo"
     end
 
-    it "decreases the quality value by 1 when a day goes by" do
-      items = [Item.new("Item", 5, 10)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 9
-    end
+    context 'for regular items' do
 
-    it "decreases the sell_in value by 1 when a day goes by" do
-      items = [Item.new("Item", 5, 10)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].sell_in).to eq 4
-    end
+      it "decreases the quality value by 1 when a day goes by" do
+        items = [Item.new("Item", 5, 10)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 9
+      end
 
+      it "decreases the sell_in value by 1 when a day goes by" do
+        items = [Item.new("Item", 5, 10)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq 4
+      end
+
+    end
 
   end
-
 end
