09152c25d24449a397c4e57d3d02235878d8600a
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-03-07 15:59:54 +0000

    Add test: decreases sell_in value by 1 every day

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,6 +3,9 @@ require 'gilded_rose'
 describe GildedRose do
 
   describe "#update_quality" do
+    context 'for regular items' do
+
+    end
 
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
@@ -10,10 +13,16 @@ describe GildedRose do
       expect(items[0].name).to eq "foo"
     end
 
-    it "decreases the quality of standard items by 1 when a day goes by" do
+    it "decreases the quality value by 1 when a day goes by" do
       items = [Item.new("Item", 5, 10)]
       GildedRose.new(items).update_quality()
       expect(items[0].quality).to eq 9
+    end
+
+    it "decreases the sell_in value by 1 when a day goes by" do
+      items = [Item.new("Item", 5, 10)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq 4
     end
 
 
