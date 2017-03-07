b7f219d8adead5bca17ad58b826d4cefd5c20234
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-03-07 15:56:33 +0000

    Feature is ran when running tests to see how app works. Add 1 test: testing whether quality of standard items decreases successfully.

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,11 +3,20 @@ require 'gilded_rose'
 describe GildedRose do
 
   describe "#update_quality" do
+
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
+
+    it "decreases the quality of standard items by 1 when a day goes by" do
+      items = [Item.new("Item", 5, 10)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 9
+    end
+
+
   end
 
 end
